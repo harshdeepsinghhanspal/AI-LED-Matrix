1f2e3c6426e75bf6e356153ac07059e4c545f488
Harshdeep Singh <96439130+harshdeepsinghhanspal@users.noreply.github.com>
2024-09-14 16:50:50 +0530

    Update Python_LED_Matrix.ino

--- Python_LED_Matrix.ino
+++ Python_LED_Matrix.ino
@@ -1,10 +1,10 @@
 String dataString = "";
-int px_val=4;
-int binaryMatrix[px_val][px_val]; // To store the px_val x px_val matrix
+//int px_val=4;
+int binaryMatrix[4][4]; // To store the px_val x px_val matrix
 
 //Assume there is 4x4 matrix
 //Manually remove/ add the values as required
-int led[px_val][px_val] = {
+int led[4][4] = {
   {0, 4, 8, A1},
   {1, 5, 9, A0},
   {2, 6, 10, 13},
@@ -13,8 +13,8 @@ int led[px_val][px_val] = {
 
 void setup() {
   Serial.begin(9600);
-  for (int i = 0; i < px_val; i++) {
-        for (int j = 0; j < px_val; j++) {
+  for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
           pinMode(led[i][j], OUTPUT);
         }
       }
@@ -35,7 +35,7 @@ void loop() {
         dataString = dataString.substring(index + 1);
         
         col++;
-        if (col == px_val) {
+        if (col == 4) {
           col = 0;
           row++;
         }
@@ -45,8 +45,8 @@ void loop() {
 
       dataString = "";
 
-      for (int i = 0; i < px_val; i++) {
-        for (int j = 0; j < px_val; j++) {
+      for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
           if(binaryMatrix[i][j]=='1'){
             digitalWrite(led[i][j], HIGH);
           }
